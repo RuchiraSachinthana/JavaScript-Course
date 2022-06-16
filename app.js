831caee9a94640ed24d77b84b3944ca38c3b4a5d
Ruchira Sachinthana <ruchira.sachinthana.1@gmail.com>
2022-06-17 01:12:24 +0530

    updated

---
+++ app.js
@@ -0,0 +1,18 @@
+console.log("hello");
+console.log(1234);
+
+var greeting = "hello";
+console.log(greeting);
+console.log([1,2,3,4,5]);
+console.table({a:1, b:2});
+
+console.error("error ");
+console.clear();
+console.warn("warn");
+console.time("hello");
+console.log('abcd');
+console.log('abcd');
+console.log('abcd');
+console.log('abcd');
+console.log('abcd');console.log('abcd');
+console.timeEnd("hello");
