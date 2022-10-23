e40b06c0658b7895bbbb51c57fb111557b7ed7d2
Ruchira Sachinthana <ruchira.sachinthana.1@gmail.com>
2022-10-24 00:06:07 +0530

    Update scriptOne.js

---
+++ scriptOne.js
@@ -0,0 +1,12 @@
+const name = "Ruchira Sachinthana";
+const age = 30;
+const job = "Web Developer";
+const city = "Colombo";
+let html;
+
+// without template strings (es5)
+
+html = "<ul><li>Name: " + name + "</li>" +
+       "<li>Age: " + age + "</li>" +
+       "<li>Job: " + job + "</li>" +
+       "<li>City: " + city + "</li></ul>";
